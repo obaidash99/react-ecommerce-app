d510e7bd6e0ff2ac85c28e2910a21cf951a779ac
obaida <obaidashurbaji99@gmail.com>
2022-12-13 19:41:22 +0200

    continue working

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -1,11 +1,45 @@
 import styled from 'styled-components';
 import { categories } from '../data';
 
-const Container = styled.div``;
-const Image = styled.img``;
-const Info = styled.div``;
-const Title = styled.h2``;
-const Button = styled.button``;
+const Container = styled.div`
+	flex: 1;
+	margin: 3px;
+	height: 70vh;
+	position: relative;
+`;
+const Image = styled.img`
+	width: 100%;
+	height: 100%;
+	object-fit: cover;
+	border-radius: 5px;
+`;
+const Info = styled.div`
+	position: absolute;
+	width: 100%;
+	height: 100%;
+	top: 0;
+	left: 0;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-direction: column;
+`;
+const Title = styled.h2`
+	color: #fffff5;
+	margin-bottom: 20px;
+	text-transform: uppercase;
+`;
+const Button = styled.button`
+	border: none;
+	padding: 10px;
+	text-transform: uppercase;
+	letter-spacing: 1px;
+	border-radius: 5px;
+	cursor: pointer;
+	background-color: #fff;
+	color: gray;
+	font-weight: bold;
+`;
 
 const CategoryItem = ({ item }) => {
 	return (
